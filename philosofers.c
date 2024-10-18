c4fa33327d34f61921a78682a0e79fcc24698e0a
Mohammed Amine Es-Salhi <mes-salh@e3r11p2.1337.ma>
2024-10-18 10:30:06 +0100

    parsing

--- philosofers.c
+++ philosofers.c
@@ -6,7 +6,7 @@
 /*   By: mes-salh <mes-salh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 09:38:34 by mes-salh          #+#    #+#             */
-/*   Updated: 2024/10/18 10:25:00 by mes-salh         ###   ########.fr       */
+/*   Updated: 2024/10/18 10:29:17 by mes-salh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,21 @@ void	init_philo(t_philo *philo, char **av, int ac)
 {
 	if (ac == 5 || ac == 6)
 	{
-		philo->philo_nbr = ft_atoi(av[1]);
-		philo->t_die = ft_atoi(av[2]);
-		philo->t_eat = ft_atoi(av[3]);
-		philo->t_sleep = ft_atoi(av[4]);
-		philo->meals_nbr = -1;
-		if (ac == 6)
+		if (ft_isnbr(av[1]) && ft_isnbr(av[2]) && ft_isnbr(av[3])
+			&& ft_isnbr(av[4]))
 		{
-			if (ft_isnbr(av[5]))
+			philo->philo_nbr = ft_atoi(av[1]);
+			philo->t_die = ft_atoi(av[2]);
+			philo->t_eat = ft_atoi(av[3]);
+			philo->t_sleep = ft_atoi(av[4]);
+			if (ac == 6 && ft_isnbr(av[5]))
 				philo->meals_nbr = ft_atoi(av[5]);
 			else
-				pars_error();
+				philo->meals_nbr = -1;
+			check_values(philo);
 		}
-		check_values(philo);
+		else
+			pars_error();
 	}
 }
 
